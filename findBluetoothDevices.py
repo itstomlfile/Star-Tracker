f810253e96ff056387a8551992dd204cd0c6731f
Tom <tom.charlesworth1997@gmail.com>
2019-12-03 01:51:57 +0000

    Implement GUI

--- findBluetoothDevices.py
+++ findBluetoothDevices.py
@@ -1,27 +1,30 @@
 import bluetooth
+from tkinter import *
 
 
 def list_devices():
     devices = bluetooth.discover_devices()
-
+    out = ""
     if devices:
-        print("Nearby devices: ")
         for bdaddr in devices:
             device_name = bluetooth.lookup_name(bdaddr)
             device_addr = bdaddr
-            print("Device Name: " + device_name + " Device Address: " + device_addr)
+            out = "Device Name: " + device_name + " Device Address: " + device_addr
     else:
-        print("No devices found")
+        out = "No devices found"
+    return out
+
+
+def init_gui():
+    root = Tk()
+    out = list_devices()
+    title_label = Label(root, text=out)
+    title_label.pack()
+    root.mainloop()
 
 
 def main():
-    list_devices()
-
-# print("Attempting to establish a connection to " + device_name)
-# try:
-#     sock = bluetooth.BluetoothSocket()
-# except bluetooth.BluetoothError:
-#     print("An error has occured!")
+    init_gui()
 
 
 if __name__ == "__main__":
