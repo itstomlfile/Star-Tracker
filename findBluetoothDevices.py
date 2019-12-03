cfe58c094ceef37fa9b60ec76acc3b339f1a0d33
Tom <tom.charlesworth1997@gmail.com>
2019-12-03 02:49:26 +0000

    Implement button

--- findBluetoothDevices.py
+++ findBluetoothDevices.py
@@ -1,26 +1,43 @@
 import bluetooth
 from tkinter import *
 
+device_addr = ""
+
 
 def list_devices():
-    devices = bluetooth.discover_devices()
-    out = ""
-    if devices:
-        for bdaddr in devices:
-            device_name = bluetooth.lookup_name(bdaddr)
-            device_addr = bdaddr
-            out = "Device Name: " + device_name + " Device Address: " + device_addr
-    else:
-        out = "No devices found"
-    return out
+    device_list = []
+    print("Performing inquiry...")
+
+    nearby_devices = bluetooth.discover_devices(duration=8, lookup_names=True,
+                                                flush_cache=True, lookup_class=False)
+
+    print("Found {} devices".format(len(nearby_devices)))
+
+    for addr, name in nearby_devices:
+        try:
+            device_list.append("   {} - {}".format(addr, name))
+        except UnicodeEncodeError:
+            print("   {} - {}".format(addr, name.encode("utf-8", "replace")))
+    return device_list
 
 
 def init_gui():
     root = Tk()
-    out = list_devices()
-    title_label = Label(root, text=out)
-    title_label.pack()
+    dev_list = list_devices()
+
+    lb = Listbox(root, width=250)
+    i = 0
+    for dev in dev_list:
+        lb.insert(i, dev)
+        i = i + 1
+    select_button = Button(root, text="Select Device", command=select)
+    lb.pack()
+    select_button.pack()
     root.mainloop()
+
+
+def select():
+    print("Test")
 
 
 def main():
